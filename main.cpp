7bcb7b3be9553e4567accdd90522e9987a0a8b64
UserStudios1 <lausitzkind52@gmail.com>
2024-10-03 14:01:27 +0200

    Added Position and Normals Comments at main.cpp (66)

--- main.cpp
+++ main.cpp
@@ -63,6 +63,7 @@ int main() {
 	std::cout << "Initialization done.\n" << std::endl;
 
 	float vertices[] = {
+		// position			  // normals
 		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
